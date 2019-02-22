0b284a594528c977aafc50f9e38e90e38117cf27
Neefay <notnife@hotmail.com>
2019-02-22 04:49:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -41,16 +41,6 @@
 	app.use(express.static(path.join(__dirname, 'public')));
 	app.use(express.static(path.join(__dirname, 'uploads')));
 
-	function createFolders() {
-		var fs = require('fs'),
-			path = require('path');
-		try {
-			fs.mkdirSync('uploads/images/avatars/players');
-		} catch (err) { if (err.code !== 'EEXIST') throw err; }
-	}
-
-	createFolders();
-
 	// SETS UP ROUTES
 	routes.setup(app, express);
 
