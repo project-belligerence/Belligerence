a10f3eea026338602b82dccaa3780924cc2c71de
Neefay <notnife@hotmail.com>
2019-02-21 23:36:50 -0300

    Update index.js

--- modules/index.js
+++ modules/index.js
@@ -18,24 +18,24 @@
   	function getModels() {
   		var db = {};
 
-	  	folders.forEach(function(file) {
-	  		var modPath = path.join(__dirname, file),
-	  			folder = file;
-	  		fs.readdirSync(modPath).filter(function(file) { return (file == "model.js"); })
-	  		.forEach(function(file) {
-	  			var modelImport = sequelize.import(path.join(modPath, file));
-	  			db[folder] = modelImport;
-			});
-	  	});
+	 //  	folders.forEach(function(file) {
+	 //  		var modPath = path.join(__dirname, file),
+	 //  			folder = file;
+	 //  		fs.readdirSync(modPath).filter(function(file) { return (file == "model.js"); })
+	 //  		.forEach(function(file) {
+	 //  			var modelImport = sequelize.import(path.join(modPath, file));
+	 //  			db[folder] = modelImport;
+		// 	});
+	 //  	});
 
-		Object.keys(db).forEach(function(modelName) {
-			if ("associate" in db[modelName]) {
-				db[modelName].associate(db);
-			}
-		});
+		// Object.keys(db).forEach(function(modelName) {
+		// 	if ("associate" in db[modelName]) {
+		// 		db[modelName].associate(db);
+		// 	}
+		// });
 
-		db.sequelize = sequelize;
-		db.Sequelize = Sequelize;
+		// db.sequelize = sequelize;
+		// db.Sequelize = Sequelize;
 
 		sequelize.sync();
 
