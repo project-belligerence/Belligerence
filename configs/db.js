76e96d0d8542cb26b4184f845ee8adccbf759f0e
Neefay <notnife@hotmail.com>
2019-02-24 14:10:57 -0300

    Update db.js

--- configs/db.js
+++ configs/db.js
@@ -29,8 +29,7 @@
 					port: process.env.DB_PORT,
 					dialect: process.env.DB_PROTOCOL,
 					host: process.env.ADDRESS,
-					sync: { force: false },
-					pool: { max: 10, min: 1, idle: 1000 }
+					pool: { max: process.env.DB_MAX_POOL, min: 1, idle: 1000 }
 				},
 				sequelize = new Sequelize(this.server, this.cred.user, this.cred.password, options),
 				debugDB = false;
